b13595d5a34767a131d806b2a0e978ff0fc112cc
tufayl <ttttufayl@gmail.com>
2021-03-23 00:46:25 +0000

    add

--- httpd/graph.go
+++ httpd/graph.go
@@ -12,6 +12,7 @@ func RetrieveShortestPath(w http.ResponseWriter, r *http.Request, db *db.DB) {
 	origin := r.URL.Query().Get("origin")
 	destination := r.URL.Query().Get("destination")
 
+	//fmt.Println(origin,destination)
 	var path types.ShortestPath
 	var err error
 
